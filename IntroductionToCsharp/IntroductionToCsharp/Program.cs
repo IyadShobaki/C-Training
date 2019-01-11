d5c2ebfb60eaa418c0612de867ef08abc012da53
Iyad Shobaki <iyadshobaki82@gmail.com>
2019-01-10 23:28:02 -0500

    first commit

---
+++ IntroductionToCsharp/IntroductionToCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        string name = "\"Program\"\nOne\nTwo";
+        Console.WriteLine(name);
+
+        //string name1 = "C:\\Program\\DotNet\\Training\\C#";
+        string name1 = @"C:\Program\DotNet\Training\C#";
+        Console.WriteLine(name1);
+        Console.ReadLine();
+
+        //-------------------------------------
+
+        //Console.WriteLine("Min = {0}", int.MinValue);
+        //Console.WriteLine("Max = {0}", int.MaxValue);
+
+
+        //Console.WriteLine("Please enter your first name");
+        //string firstName = Console.ReadLine();
+
+        //Console.WriteLine("Please enter your last name");
+        //string lastName = Console.ReadLine();
+
+        //Console.WriteLine("Hello {0} {1}", firstName, lastName);
+
+        ////Console.WriteLine("Hello " + userName);
+
+        //Console.ReadLine();
+    }
+
+}
+
